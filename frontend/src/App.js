971722507ab6c7e686670d8324124da9abf83682
ampersand-paris <ampersand.s.paris@gmail.com>
2025-03-06 11:55:04 -0500

    footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 
-
 // COMPONENTS
 import Nav from './components/Nav';
+import appHeight from './appHeight';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
       <Nav />
+      <Footer />
     </>
   );
 }
